b3c96755d33a28f07578486caf5885ca1a3934fc
ipincamp <arif.lngsrn@gmail.com>
2024-04-22 22:31:33 +0700

    Fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <style>
+        .frame {
+            border: 1px solid #aaa;
+        }
+    </style>
 
     <!-- Fontawesome JS -->
     <script src="https://kit.fontawesome.com/1aa10d38c2.js" crossorigin="anonymous"></script>
@@ -23,87 +28,118 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'SIAKAD') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <div class="container-fluid">
+            {{-- !Navbar --}}
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="{{ url('/') }}">
+                                {{ config('app.name', 'SIAKAD') }}
+                            </a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    @if (Auth::check())
-                        <ul class="nav nav-underline">
-                            <li class="nav-item">
-                                <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}"
-                                    href="{{ route('home') }}">{{ __('Home') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link {{ request()->routeIs('data-mahasiswa') ? 'active' : '' }}"
-                                    href="{{ route('data-mahasiswa') }}">{{ __('Data Mahasiswa') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link {{ request()->routeIs('jadwal-kuliah') ? 'active' : '' }}"
-                                    href="{{ route('jadwal-kuliah') }}">{{ __('Jadwal Perkuliahan') }}</a>
-                            </li>
-                        </ul>
-                    @endif
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <!-- Left Side Of Navbar -->
+                                @if (Auth::check())
+                                    <ul class="nav nav-underline">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                                    </ul>
+                                @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                                <!-- Right Side Of Navbar -->
+                                <ul class="navbar-nav ms-auto">
+                                    <!-- Authentication Links -->
+                                    @guest
+                                        @if (Route::has('login'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                            </li>
+                                        @endif
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                        @if (Route::has('register'))
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                            </li>
+                                        @endif
+                                    @else
+                                        <li class="nav-item dropdown">
+                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                                role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false" v-pre>
+                                                {{ Auth::user()->name }}
+                                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                                    onclick="event.preventDefault();
+                                                                document.getElementById('logout-form').submit();">
+                                                    {{ __('Logout') }}
+                                                </a>
+
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                                    class="d-none">
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </li>
+                                    @endguest
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
             </div>
-        </nav>
+            {{-- !Navbar --}}
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+            {{-- !Body --}}
+            <main class="row">
+                {{-- *Sidebar --}}
+                <div class="col-md-2">
+                    <div class="list-group mt-3">
+                        <a href="{{ route('home') }}"
+                            class="list-group-item list-group-item-action {{ request()->routeIs('home') ? 'active' : '' }}">
+                            <i class="fas fa-home"></i> <span class="d-none d-sm-inline">Home</span>
+                        </a>
+                        <a href="{{ route('data-mahasiswa') }}"
+                            class="list-group-item list-group-item-action {{ request()->routeIs('data-mahasiswa') ? 'active' : '' }}">
+                            <i class="fas fa-users"></i> <span class="d-none d-sm-inline">Data Mahasiswa</span>
+                        </a>
+                        <a href="{{ route('jadwal-kuliah') }}"
+                            class="list-group-item list-group-item-action {{ request()->routeIs('jadwal-kuliah') ? 'active' : '' }}">
+                            <i class="fas fa-calendar-alt"></i> <span class="d-none d-sm-inline">Jadwal
+                                Perkuliahan</span>
+                        </a>
+                    </div>
+                </div>
+                {{-- *Sidebar --}}
 
-        <footer class="footer mt-auto py-2 bg-light fixed-bottom text-center">
-            <div class="container">
-                <span class="text-muted">
-                    &copy; {{ date('Y') }} {{ config('app.name', 'SIAKAD') }}. All rights reserved.
-                </span>
+                {{-- *Content --}}
+                <div class="col-md-10">
+                    @yield('content')
+                </div>
+                {{-- *Content --}}
+            </main>
+            {{-- !Body --}}
+
+            {{-- !Footer --}}
+            <div class="row">
+                <div class="col-md-12">
+                    <footer class="footer mt-auto py-2 bg-light fixed-bottom text-center">
+                        <div class="container">
+                            <span class="text-muted">
+                                &copy; {{ date('Y') }} {{ config('app.name', 'SIAKAD') }}. All rights reserved.
+                            </span>
+                        </div>
+                    </footer>
+                </div>
             </div>
-        </footer>
+            {{-- !Footer --}}
+        </div>
     </div>
 </body>
 
